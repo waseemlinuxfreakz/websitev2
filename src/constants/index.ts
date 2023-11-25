2bf3b6299a25e7a79c950bebe7b2a2edbd609c32
Mithras <dimabrook79@gmail.com>
2023-11-25 12:38:58 +0200

    add WalletConnect

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -9,4 +9,4 @@ export enum IrisApiUrl {
 
 export const DEFAULT_BLOCKCHAIN_DELAY = 1000 // polling every second
 export const DEFAULT_API_DELAY = 5000 // polling every 5 second
-export const TX_HASH_KEY = 'txHash'
+export const TX_HASH_KEY = 'txHash';
