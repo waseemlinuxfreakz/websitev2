fd4bfe8692995a253e12bc4f564ca01ec7936676
Mithras <dimabrook79@gmail.com>
2023-12-07 13:48:05 +0200

    on mobile show 3...3 address characters

--- src/HeaderFooterSidebar/ConnectWallet.jsx
+++ src/HeaderFooterSidebar/ConnectWallet.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import Wallet from '../assets/img/Wallet.svg';
 import { useWeb3Modal } from '@web3modal/react';
 import { useAccount } from 'wagmi';
+import {isMobile} from 'react-device-detect';
 
 export default function ConnectWallet() {
 
     const { open } = useWeb3Modal();
     const { address, isConnected } = useAccount();
+
+    const showCharacters = isMobile ? 3 : 6;
 
     return (
         <div
@@ -16,7 +19,7 @@ export default function ConnectWallet() {
             <div>
                 <img src={Wallet} alt="Wallet" />
                 {isConnected
-                ? `${address.slice(0,6)}...${address.slice(-6,)}`
+                ? `${address.slice(0,showCharacters)}...${address.slice(-showCharacters,)}`
                 : 'Connect'}
                 
             </div>
