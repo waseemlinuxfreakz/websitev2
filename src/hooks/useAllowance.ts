4b2725adbae355e3d2a62c37b12e2d8f8173a0a0
Mithras <dimabrook79@gmail.com>
2023-12-10 18:17:53 +0200

    add: useAllowance hook preparatory file

---
+++ src/hooks/useAllowance.ts
@@ -0,0 +1,12 @@
+import { useEffect, useState } from 'react';
+
+export default function useBridgeAllowance() {
+
+    const [allowance, setAllowance] = useState(0);
+
+    useEffect(() => {
+
+    }, []);
+
+    return allowance;
+}
