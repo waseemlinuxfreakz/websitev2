ac8ea99064ff46c36b130eab9b631af2984981e9
Mithras <dimabrook79@gmail.com>
2023-12-12 10:22:02 +0200

    fix: # of transactions wording

--- src/Pages/ExplorerComponents/ExplorerTransactions/ExplorerTransactionsTitle.jsx
+++ src/Pages/ExplorerComponents/ExplorerTransactions/ExplorerTransactionsTitle.jsx
@@ -5,7 +5,7 @@ function ExplorerTransactionsTitle() {
     return ( 
         <div className="explorerTransactionsTitle">
             <h2>Transactions</h2>
-            <p>Total 1,163,132 transactions within the latest 24 hour</p>
+            <p>Total 1,163,132 TXs in 24 hours</p>
         </div>
      );
 }
