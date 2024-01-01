04c55cb347c5ac599c92abe834264533661d8df5
Mithras <dimabrook79@gmail.com>
2024-01-01 13:02:37 +0200

    fix: Links do not have a discernible name

--- src/HeaderFooterSidebar/NavSocial.jsx
+++ src/HeaderFooterSidebar/NavSocial.jsx
@@ -7,16 +7,60 @@ import './NavSocial.css';
 
 
 function NavSocial() {
-    return ( 
+    return (
         <div className="social_box">
             <ul className="socialList">
-                <li><a href="https://twitter.com/Emmet_Finance" target="_blank"><img src={Xtwiter} alt="" /></a></li>
-                <li><a href="https://github.com/Emmet-Finance/" target="_blank"><img src={Github} alt="" /></a></li>
-                <li><a href="https://discord.com/invite/yuXxDctC4W" target="_blank"><img src={Discord} alt="" /></a></li>
-                <li><a href="https://t.me/Emmet_Finance" target="_blank"><img src={Telegram} alt="" /></a></li>
+                <li>
+                    <a
+                        aria-label="Go to the corporate X (former Twitter) channel"
+                        href="https://twitter.com/Emmet_Finance"
+                        target="_blank"
+                    >
+                        <img
+                            src={Xtwiter}
+                            alt="X (former Twitter) Logo"
+                        />
+                    </a>
+                </li>
+                <li>
+                    <a
+                        aria-label="Go to the corporate GitHub repository"
+                        href="https://github.com/Emmet-Finance/"
+                        target="_blank"
+                    >
+                        <img
+                            src={Github}
+                            alt="GitHub Logo"
+                        />
+                    </a>
+                </li>
+                <li>
+                    <a
+                        aria-label="Go to the corporate Discord channel"
+                        href="https://discord.com/invite/yuXxDctC4W"
+                        target="_blank"
+                    >
+                        <img
+                            src={Discord}
+                            alt="Discord Logo"
+                        />
+                    </a>
+                </li>
+                <li>
+                    <a
+                        aria-label="Go to the corporate Telegram channel"
+                        href="https://t.me/Emmet_Finance"
+                        target="_blank"
+                    >
+                        <img
+                            src={Telegram}
+                            alt="Telegram Logo"
+                        />
+                    </a>
+                </li>
             </ul>
         </div>
-     );
+    );
 }
 
 export default NavSocial;
