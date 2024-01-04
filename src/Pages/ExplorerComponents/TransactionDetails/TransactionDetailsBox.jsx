7ba2a34467bcfad12081eb768e78d497cff22c7b
Mithras <dimabrook79@gmail.com>
2024-01-04 18:19:48 +0200

    call: useBridgingFee & propagate { fromFee, toFee, status } to children

--- src/Pages/ExplorerComponents/TransactionDetails/TransactionDetailsBox.jsx
+++ src/Pages/ExplorerComponents/TransactionDetails/TransactionDetailsBox.jsx
@@ -1,22 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import TransactionDetailsLeft from './TransactionDetailsLeft';
 import TransactionDetailsRight from './TransactionDetailsRight';
+import { useAppSelector } from '../../../hooks/storage';
+import useBridgingFee from '../../../hooks/useBridgingFee';
+import { getDomainToChainName } from '../../../utils';
 
+export default function TransactionDetailsArea() {
 
+    // Bridge transaction
+    const btx = useAppSelector(store => store.explorer.bridgeTransaction);
 
-function TransactionDetailsArea() {
+    const { fromFee, toFee, status } = useBridgingFee(
+        btx.burnHash,
+        getDomainToChainName(btx.originalDomain),
+        btx.claimHash,
+        getDomainToChainName(btx.destinationDomain)
+    );
+
     return ( 
         <div className="transactionDetailsArea">
             <div className="row">
                 <div className="col-lg-6">
-                    <TransactionDetailsLeft/>
+                    <TransactionDetailsLeft
+                        txStatus={status}
+                    />
                 </div>
                 <div className="col-lg-6">
-                    <TransactionDetailsRight/>
+                    <TransactionDetailsRight
+                        fromFee={fromFee}
+                        toFee={toFee}
+                    />
                 </div>
             </div>
         </div>
      );
 }
-
-export default TransactionDetailsArea;
