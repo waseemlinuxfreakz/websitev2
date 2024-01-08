7ff2070429254b6cb26e443f146a101ba59fe8e9
Mithras <dimabrook79@gmail.com>
2024-01-08 18:13:09 +0200

    upgrade & stabilize Approve hook

--- src/hooks/useBridgeApproveERC20.ts
+++ src/hooks/useBridgeApproveERC20.ts
@@ -1,4 +1,4 @@
-import { addressToAccount, getTokenAddress } from '../utils';
+import { addressToAccount, getProvider, getTokenAddress, sleep } from '../utils';
 import { TChainName, TTokenName, ChainNameToTypeChainName, CHAIN_NAME_TO_ID, SUPPORTED_CHAINS } from '../types';
 import {
     erc20ABI,
@@ -6,10 +6,26 @@ import {
     usePrepareContractWrite,
     useAccount,
 } from 'wagmi';
-import { Hash } from 'viem';
-import { setBridgeError, setBridgeAllowance } from '../store/bridgeSlice';
+import { Hash, TransactionReceipt } from 'viem';
+import { setBridgeError, setBridgeAllowance, setBridgeIsApproving } from '../store/bridgeSlice';
 import { useAppDispatch, useAppSelector } from './storage';
 import { useEffect, useState } from 'react';
+
+async function getTxReceipt(hash: string, chainName: TChainName): Promise<TransactionReceipt> {
+
+    try {
+
+        const provider = getProvider(chainName);
+        const TX: TransactionReceipt = await provider.getTransactionReceipt({ hash: `0x${hash.replace('0x', '')}` });
+
+        return TX;
+
+    } catch (error) {
+        await sleep(3000);
+        return getTxReceipt(hash, chainName);
+    }
+
+}
 
 
 export default function useBridgeApproveERC20() {
@@ -19,7 +35,6 @@ export default function useBridgeApproveERC20() {
     const dispatch = useAppDispatch();
 
     const bridge = useAppSelector((state) => state.bridge);
-
 
     const [tokenName, setTokenName] = useState<TTokenName>(bridge.fromToken as TTokenName);
 
@@ -34,8 +49,6 @@ export default function useBridgeApproveERC20() {
     const [formattedAmount, setFormattedAmount] = useState<number>(Number(bridge.amount) * 10 ** Number(decimals.toString()));
 
     const [spender, setSpender] = useState<string>(SUPPORTED_CHAINS[chainName].bridge);
-
-
 
     useEffect(() => {
         setFormattedAmount(Number(bridge.amount) * 10 ** decimals)
@@ -64,7 +77,6 @@ export default function useBridgeApproveERC20() {
         setFormattedAmount(Number(bridge.amount) * 10 ** decimals)
     }, [bridge.decimals]);
 
-    // @ts-ignore
     const { config } = usePrepareContractWrite({
         address: addressToAccount(tokenAddress),
         account: address,
@@ -74,13 +86,27 @@ export default function useBridgeApproveERC20() {
         chainId,
         onError(err: { message: string | undefined; }) {
             dispatch(setBridgeError(err.message));
+            dispatch(setBridgeIsApproving(false));
+        },
+        onSettled() {
+            dispatch(setBridgeError(""));
         }
     });
 
     const { data, isLoading, isSuccess, write } = useContractWrite(config);
 
-    if(isSuccess){
-        dispatch(setBridgeAllowance(Number(bridge.amount) * 10 ** decimals));
+    if (data && isSuccess ) {
+        (async () => {
+            const TX = await getTxReceipt(data.hash, chainName);
+
+            if (TX && TX.status === 'success') {
+                await sleep(20_000);
+                dispatch(setBridgeAllowance(Number(bridge.amount) * 10 ** decimals));
+                dispatch(setBridgeIsApproving(false));
+            }
+
+        })();
+
     }
 
     return { approveData: data, isApproveLoading: isLoading, isApproveSuccess: isSuccess, approve: write };
