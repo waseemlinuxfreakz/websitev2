bb666210728c25e555f47029de657cbaf9bd2ba9
Mithras <dimabrook79@gmail.com>
2024-01-08 11:39:53 +0200

    add spinner component

---
+++ src/Pages/CommonComponents/Spinner/ButtonSpinner.jsx
@@ -0,0 +1,21 @@
+import react from 'react';
+import './ButtonSpinner.css';
+
+export default function ButtonSpinner() {
+    return (
+        <div className="lds-ring">
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+        </div>
+    );
+}
