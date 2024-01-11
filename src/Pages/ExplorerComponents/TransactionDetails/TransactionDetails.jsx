9ba8fcc6ebffe0339930d7b19cb545eb7f3dd60a
Mithras <dimabrook79@gmail.com>
2024-01-11 10:57:07 +0200

    fix: TransactionDetails opens in the middle

--- src/Pages/ExplorerComponents/TransactionDetails/TransactionDetails.jsx
+++ src/Pages/ExplorerComponents/TransactionDetails/TransactionDetails.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import TransactionDetailsBreadcrumb from './TransactionDetailsBreadcrumb';
 import TransactionHash from './TransactionHash';
 import TransactionDetailsArea from './TransactionDetailsBox';
 
 
 function TransactionDetails() {
+
+    const [scrolledTop, setScrolledTop] = useState(false);
+
+    if(!scrolledTop){
+        window.scroll(0,0);
+        setScrolledTop(true);
+    }
+
     return ( 
         <div className="TransactionDetailsArea">
             <TransactionDetailsBreadcrumb/>
