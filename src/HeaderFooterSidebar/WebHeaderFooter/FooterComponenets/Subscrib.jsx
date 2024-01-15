0da1111f0ff2ac919cf944caa3abca25a000c8d5
Mithras <dimabrook79@gmail.com>
2024-01-15 15:20:30 +0200

    add email input validation

--- src/HeaderFooterSidebar/WebHeaderFooter/FooterComponenets/Subscrib.jsx
+++ src/HeaderFooterSidebar/WebHeaderFooter/FooterComponenets/Subscrib.jsx
@@ -1,17 +1,46 @@
-import React from 'react';
-
-import './Subscrib.css'
+import React, {useState} from 'react';
+import './Subscrib.css';
+import { isValidEmail } from '../../../verifiers';
 
 function Subscrib() {
-    return ( 
+
+    const [value, setValue] = useState('');
+
+    const handleEmailChange = (e) => {
+        e.preventDefault();
+        const rawInput = e.target.value;
+        const sanitized = rawInput.replace(/[^a-zA-Z0-9@._-]/g, '');
+        setValue(sanitized);
+    }
+
+    const handleSubscribeClick = () => {
+        if(isValidEmail(value)){
+            console.log(value, "is a valid email")
+        }else{
+            console.error(value, "is NOT a valid email")
+        }
+    }
+
+
+    return (
         <div className="subscribBox">
             <h2>Sign up for our newsletter and join the growing EMMET community</h2>
             <form action="#" className="subscribForm">
-                <input type="search" placeholder='Email' />
-                <button type='button'>Subscribe</button>
+                <input
+                    type="email"
+                    placeholder='Email'
+                    onChange={e => handleEmailChange(e)}
+                    value={value}
+                />
+                <button
+                    type='button'
+                    onClick={handleSubscribeClick}
+                >
+                    Subscribe
+                </button>
             </form>
         </div>
-     );
+    );
 }
 
 export default Subscrib;
