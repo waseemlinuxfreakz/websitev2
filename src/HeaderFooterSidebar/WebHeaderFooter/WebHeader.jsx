46da2a21dd9b80bc404c4294964f05107ccf96a3
waseemlinuxfreakz <89018806+waseemlinuxfreakz@users.noreply.github.com>
2024-01-15 21:08:55 +0600

    Feedback
    Feedback

--- src/HeaderFooterSidebar/WebHeaderFooter/WebHeader.jsx
+++ src/HeaderFooterSidebar/WebHeaderFooter/WebHeader.jsx
@@ -2,18 +2,23 @@ import React from 'react';
 
 import Logo1 from '../../assets/img/web/logo.svg';
 import Logo2 from '../../assets/img/web/logo-2.svg';
+import LogoMob from '../../assets/img/web/logo-mob.svg';
 
 import './WebHeader.css'
 function WebHeader() {
+    const isMobile = window.innerWidth <= 768;
     return (
         <header className='webHeader'>
             <div className="container">
                 <div className="navArea">
                     <div className="navBrand">
-                        <a href="#">
-                            <img src={Logo1} className='logo-1' alt="Logo" />
-                            <img src={Logo2} className='logo-2' alt="Logo" />
-                        </a>
+                        {isMobile ? null : 
+                            <a href="./">
+                                <img src={Logo1} className='logo-1' alt="Logo" />
+                                <img src={Logo2} className='logo-2' alt="Logo" />
+                            </a>
+                        }
+                        <a href="./" className='mobLogo'><img src={LogoMob} alt="Emmet" className='' /></a>
                     </div>
                         <a href="/bridge" className='launchApp'>Launch dApp</a>
                 </div>
