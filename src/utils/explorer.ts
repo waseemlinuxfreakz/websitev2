cd9f450204f5a22002efd3f109e453840be15ff3
Mithras <dimabrook79@gmail.com>
2024-02-04 20:48:22 +0200

    create getSearchDataType

---
+++ src/utils/explorer.ts
@@ -0,0 +1,18 @@
+import { SearchDataType } from "../types";
+import { isBridgeTransaction, isEvmAddress, isEvmTransactionHash } from "./address";
+import { isTokenName } from "./tokens";
+
+export function getSearchDataType(data: string): SearchDataType {
+    switch (true) {
+        case isTokenName(data):
+            return SearchDataType.Token;
+        case isEvmAddress(data):
+            return SearchDataType.EvmAddress;
+        case isEvmTransactionHash(data):
+            return SearchDataType.EvmTransaction;
+        case isBridgeTransaction(data):
+            return SearchDataType.BridgeTransaction;
+        default:
+            return SearchDataType.None;
+    }
+}
