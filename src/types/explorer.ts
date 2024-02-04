07b03e3dbf660b07984146d3e823205078472ee2
Mithras <dimabrook79@gmail.com>
2024-02-04 19:44:37 +0200

    create: explorer-related types

---
+++ src/types/explorer.ts
@@ -0,0 +1,83 @@
+
+export type TExplorerFilter = 'NONE' | 'SENDER' | 'FROMCHAIN' | 'TOCHAIN';
+
+export const ROWS_PER_PAGE = 10;
+
+export const columns = [
+    {
+      label: 'Txn Type',
+      field: 'TxnType',
+      sort: 'asc',
+    },
+    {
+      label: 'Txn Hash',
+      field: 'TxnHash',
+      sort: 'asc',
+    },
+    {
+      label: 'Origin chain',
+      field: 'Originchain',
+      sort: 'asc',
+    },
+    {
+      label: 'Destination',
+      field: 'Destination',
+      sort: 'asc',
+    },
+    {
+      label: 'Sent',
+      field: 'Sent',
+      sort: 'asc',
+    },
+    {
+      label: 'Received',
+      field: 'Received',
+      sort: 'asc',
+    },
+    {
+      label: 'Age',
+      field: 'Age',
+      sort: 'asc',
+    },
+    {
+      label: 'Txn Status',
+      field: 'TxnStatus',
+      sort: 'asc',
+    }
+  ];
+
+  export type TTxType =
+    "Approve"
+    | "Borrow"
+    | "Deposit"
+    | "Redem"
+    | "Swap"
+    | "Transfer";
+
+export type TTxStatus =
+    "Failed"
+    | "Pending"
+    | "Reverted"
+    | "Success";
+
+export type TxDetails = {
+    age?: number,
+    amount?: number, // number of transferred tokens
+    bridgeFee: number,
+    bridgeHash: string,
+    burnToken?: string, // the address of the transferred token
+    mintRecipient?: string, // Receiver address
+    destinationFee?: number,
+    destinationDomain: number, // Number 0-7, Ethereum == 0, Polygon == 7
+    // Additional params
+    originalDomain: number,
+    originFee: number,
+    sender: string,
+    burnHash: string,
+    start?: Date,
+    symbol: string,
+    claimHash?: string, // Will arrive when the token is claimed
+    finished?: Date
+    txType?: TTxType,
+    status?: TTxStatus
+}
