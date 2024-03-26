e6bb0e575afffea1198a54d9602c7c11edb69b88
Mithras <dimabrook79@gmail.com>
2024-03-26 11:02:30 +0200

    fix the bridging fee

--- src/utils/numbers.ts
+++ src/utils/numbers.ts
@@ -5,7 +5,7 @@ export function restoreOriginalSumSent(amount: number) {
     if (amount < 0) return 0;
 
     if (amount <= 2000) {
-        return Number(amount + 0.4).toFixed(8);
+        return Number(amount + 0.05).toFixed(8);
     }
 
     return Number(amount + amount * 0.0002).toFixed(8);
