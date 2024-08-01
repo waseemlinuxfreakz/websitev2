15e22cefb58be026f6cbffbe349e8dd36e7af5cd
Rishabh Kumar <rsbkme@gmail.com>
2024-08-02 00:09:00 +0530

    fix table on mobile

--- src/Pages/PoolComponents/PoolTable/PoolMobileData.jsx
+++ src/Pages/PoolComponents/PoolTable/PoolMobileData.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
-import USDT from "../../../assets/img/coin/usdc.svg";
-import ETH from "../../../assets/img/coin/eth.svg";
 import Yourliquidity from "./Yourliquidity";
 import { Link } from "react-router-dom";
 import { useAppSelector } from "../../../hooks/storage";
+import poolTokens from "../../../store/poolCoins.json";
+import poolChains from "../../../store/poolChains.json";
 
 function PoolMobileData() {
   const [isYourLiquidityVisible, setYourLiquidityVisible] = useState(false);
@@ -12,45 +12,84 @@ function PoolMobileData() {
     setYourLiquidityVisible(!isYourLiquidityVisible);
   };
   const pool = useAppSelector((state) => state.pool);
+  const bridge = useAppSelector((state) => state.bridge);
+  const getTokenIcon = (token) => {
+    return poolTokens.find((i) => i.name === token).icon;
+  };
+
+  const getChainIcon = (chain) => {
+    return poolChains.find((i) => i.name === chain).icon;
+  };
+
+  const data = [
+    {
+      token: "USDC",
+      chain: "Sepolia",
+      apy: pool.apy,
+      volume: "$43,432.00",
+      totalLiquidity: pool.totalSupply,
+    },
+    {
+      token: "USDC",
+      chain: "TONTestnet",
+      apy: 0,
+      volume: "$43,432.00",
+      totalLiquidity: 0,
+    },
+  ];
 
   return (
     <>
       <div className="poolMobileData">
-        <div className="poolBox addDepositBox">
-          <div className="poolboxTop">
-            <div className="poolboxTopLeft">
-              <div className="chainToken">
-                <img src={USDT} alt="USDT" className="mainChain" />
-                <img src={ETH} alt="ETH" className="onChain" />
+        {data.map(
+          (item, index) =>
+            bridge.fromChain === item.chain &&
+            bridge.fromToken === item.token && (
+              <div className="poolBox addDepositBox">
+                <div className="poolboxTop">
+                  <div className="poolboxTopLeft">
+                    <div className="chainToken">
+                      <img
+                        src={getTokenIcon(item.token)}
+                        alt="USDT"
+                        className="mainChain"
+                      />
+                      <img
+                        src={getChainIcon(item.chain)}
+                        alt="ETH"
+                        className="onChain"
+                      />
+                    </div>
+                    <div className="">
+                      <h2>{item.token}</h2>
+                      on {item.chain}
+                    </div>
+                  </div>
+                </div>
+                <div className="poolboxBottom">
+                  <div className="row">
+                    <div className="col-6">
+                      <h4>Total liquidity (USDC)</h4>
+                      <h3>${item.totalLiquidity}</h3>
+                    </div>
+                    <div className="col-6">
+                      <h4>APY (%)</h4>
+                      <h3>{item.apy}%</h3>
+                    </div>
+                    <div className="addDeposit">
+                      <Link
+                        to="./your-liquidity"
+                        className="addDepositBtn"
+                        onClick={handleAddPollClick}
+                      >
+                        + Add deposit
+                      </Link>
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div className="">
-                <h2>USDC</h2>
-                on Sepolia
-              </div>
-            </div>
-          </div>
-          <div className="poolboxBottom">
-            <div className="row">
-              <div className="col-6">
-                <h4>Total liquidity (USDC)</h4>
-                <h3>${pool.totalSupply}</h3>
-              </div>
-              <div className="col-6">
-                <h4>APY (%)</h4>
-                <h3>{pool.apy}%</h3>
-              </div>
-              <div className="addDeposit">
-                <Link
-                  to="./your-liquidity"
-                  className="addDepositBtn"
-                  onClick={handleAddPollClick}
-                >
-                  + Add deposit
-                </Link>
-              </div>
-            </div>
-          </div>
-        </div>
+            ),
+        )}
       </div>
       {/* {isYourLiquidityVisible && <Yourliquidity />} */}
       <div className="mobileDeposite">
