ff179cd4439e4bda7795e39ae9d8b2d4c673d5bd
Mithras <dimabrook79@gmail.com>
2024-11-10 11:35:37 +0200

    fix: approve

--- src/hooks/useBridgeApproveERC20.ts
+++ src/hooks/useBridgeApproveERC20.ts
@@ -10,7 +10,7 @@ import { useAppDispatch, useAppSelector } from "./storage";
 import { useEthersSigner } from "./useEthersSigner";
 import { chainFactory } from "../store/chainFactory";
 import { Web3Helper } from "emmet.js/dist/chains/web3";
-import { AddressBookKeys } from "emmet.js";
+import { AddressBookKeys, TonHelper } from "emmet.js";
 import { setPoolAllowance } from "../store/poolSlice";
 
 export default function useBridgeApproveERC20() {
@@ -33,12 +33,12 @@ export default function useBridgeApproveERC20() {
         // @ts-ignore
         const decimals: number = TOKEN_DECIMALS[pool.token];
         const formattedAmount = Number(pool.amount) * 10 ** decimals;
-        const handler: Web3Helper = await chainFactory.inner(
+        const handler: Web3Helper | TonHelper = await chainFactory.inner(
           // @ts-ignore
           ChainToDestinationDomain[ChainNameToTypeChainName[pool.chain]],
         ) as Web3Helper;
-        const token = await (handler as Web3Helper).token(tokenName);
-        const tokenAddress = token.token;
+        const tokenAddress = await handler.getTokenAddress(tokenName);
+
         if ("address" in handler && signer) {
           const poolAddress = await handler.address(
             `elp${pool.token}` as AddressBookKeys,
@@ -69,16 +69,13 @@ export default function useBridgeApproveERC20() {
       (async () => {
         setIsLoading(true);
         const tokenName: TTokenName = bridge.fromToken as TTokenName;
-        const decimals: number = bridge.decimals
-          ? Number(bridge.decimals.toString())
-          : 18;
+        const decimals: number = TOKEN_DECIMALS[tokenName];
         const formattedAmount = Number(bridge.amount) * 10 ** decimals;
-        const handler: Web3Helper = await chainFactory.inner(
+        const handler: Web3Helper | TonHelper = await chainFactory.inner(
           // @ts-ignore
           ChainToDestinationDomain[ChainNameToTypeChainName[bridge.fromChain]],
         ) as Web3Helper;
-        const token = await (handler).token(tokenName);
-        const tokenAddress = token.token;
+        const tokenAddress = await handler.getTokenAddress(tokenName);
         const bridgeAddress = await handler.bridge();
 
         console.log({ bridgeAddress });
